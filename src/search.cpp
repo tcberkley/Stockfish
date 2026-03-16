6841e957d491aea1fa445a41bc6f5a00d5c50b01
Tom Berkley <tomberkley@Toms-Air.lan>
2026-03-16 18:51:25 -0400

    Add material scaling to null move reduction
    Null move (passing) is worth more with more material on the board since
    tempo value scales with piece count. Add non_pawn_material/8192 to NMP
    reduction so middlegames get 0-1 extra depth of reduction while endgames
    (where null move is less reliable) stay at current levels.
    
    Bench: 2389086

--- src/search.cpp
+++ src/search.cpp
@@ -896,7 +896,7 @@ Value Search::Worker::search(
         assert((ss - 1)->currentMove != Move::null());
 
         // Null move dynamic reduction based on depth
-        Depth R = 7 + depth / 3;
+        Depth R = 7 + depth / 3 + pos.non_pawn_material(us) / 8192;
         do_null_move(pos, st, ss);
 
         Value nullValue = -search<NonPV>(pos, ss + 1, -beta, -beta + 1, depth - R, false);
