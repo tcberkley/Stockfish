fd6228a63f4955391a246c18ba909b49f68f8408
Tom Berkley <tomberkley@Toms-Air.lan>
2026-03-17 10:17:13 -0400

    Continuation history pruning: linear to quadratic
    Change contHist pruning threshold from -3826*depth (linear) to
    -956*depth*depth (quadratic). Prunes less at shallow depths where
    mistakes are costly, more at deep depths. Crossover at depth 4.
    
    Bench: 2491957

--- src/search.cpp
+++ src/search.cpp
@@ -1086,7 +1086,7 @@ moves_loop:  // When in check, search starts here
                             + sharedHistory.pawn_entry(pos)[movedPiece][move.to_sq()];
 
                 // Continuation history based pruning
-                if (history < -3826 * depth)
+                if (history < -956 * depth * depth)
                     continue;
 
                 history += 73 * mainHistory[us][move.raw()] / 32;
