c58da266c9627c30e95ca9dfde6145caca3d6e62
Tom Berkley <tomberkley@Toms-Air.lan>
2026-03-18 20:06:39 -0400

    Remove low ply history from quiet scoring
    Drop the low-ply-history overlay from quiet move ordering and rely on the remaining history stack. This tests whether the extra root-local term is now redundant noise in the main quiet scoring path.
    
    Bench: 3082890

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -174,9 +174,6 @@ ExtMove* MovePicker::score(MoveList<Type>& ml) {
             int v = 20 * (bool(threatByLesser[pt] & from) - bool(threatByLesser[pt] & to));
             m.value += PieceValue[pt] * v;
 
-
-            if (ply < LOW_PLY_HISTORY_SIZE)
-                m.value += 8 * (*lowPlyHistory)[ply][m.raw()] / (1 + ply);
         }
 
         else  // Type == EVASIONS
